448f7d46274a018c86e5921f0d3c2a6788079e9b
astin75 <astin75@gmail.com>
2023-07-14 03:44:48 +0900

    overlay python

--- utils/overlay_parser.py
+++ utils/overlay_parser.py
@@ -2,11 +2,12 @@ import yaml
 import os
 staging_root_path = "kustomize_chart/fast-api/overlays/staging"
 kustomization_path = os.path.join(staging_root_path, "kustomization.yaml")
+workflow_patches_path = os.path.join(staging_root_path, "workflow-patches.yaml")
 
 with open(kustomization_path, 'r') as f:
     kustomization = yaml.safe_load(f)
     
-print(kustomization)
+
 kustomization['namespace'] = 'production'
 kustomization['nameSuffix'] = '-production'
 
@@ -14,3 +15,11 @@ print(kustomization)
 kustomization_path_production = kustomization_path.replace("staging", "production")
 with open(kustomization_path_production, 'w') as yaml_file:
     yaml.dump(kustomization, yaml_file, sort_keys=False)
+    
+    
+with open(workflow_patches_path, 'r') as f:
+    workflow = yaml.safe_load(f)
+
+workflow_patches_path_production = workflow_patches_path.replace("staging", "production")
+with open(workflow_patches_path_production, 'w') as yaml_file:
+    yaml.dump(workflow, yaml_file, sort_keys=False)
